4e7308c2ca8b6e8027c43a339d4b73a75ef5ff5b
Pasqualino Sorice <lino.sorice@gmail.com>
2021-05-22 22:06:40 +0200

    Style footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,12 @@
 import * as React from 'react'
 
 const Footer = () => (
-  <div className='flex'>
-    <div className='flex justify-items-start h-footer'>© 2021 - All rights reserved to Giulia Segatori</div>
-
-    <img src='/envelope.png' alt='send email' className='mr-2' />
-    <img src='/linkedin_icon.png' alt='linkedin profile' />
-
+  <div className='p-8 mt-2'>
+    <div className='flex justify-center'>
+      <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/giulia-segatori' className='mr-2'><img src='/linkedin_icon.png' alt='linkedin profile' /></a>
+      <a href='mailto:giulia.segatori@gmail.com' className='ml-2'><img src='/envelope.png' alt='send email' /></a>
+    </div>
+    <div className='mt-8 text-xs text-center font-extralight text-gray-400'>© 2021 - All rights reserved to Giulia Segatori</div>
   </div>
 
 )
