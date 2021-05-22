2f96bc22a4d6af810d8a3a1eda3b93dc82d898fb
Pasqualino Sorice <lino.sorice@gmail.com>
2021-05-22 23:15:36 +0200

    add typewriter library

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,34 @@
+import Typewriter from 'typewriter-effect'
 import Layout from '../components/Layout'
 
 const PayOff = () => (
   <div className='h-payoff relative'>
-    <div className='absolute bottom-2/5 lg:px-8 px-2'>
-      <div className='font-bold lg:text-7xl text-center text-2xl'>👋 Hey there, I’m Giulia!</div>
-      <div className='lg:text-3xl lg:text-left text-center text-lg font-thin mt-12'>I am passionate about UX design</div>
+    <div className='absolute lg:bottom-2/5 bottom-1/2 lg:px-8 w-full'>
+      <h1 className='font-bold lg:text-7xl text-center text-2xl'>👋 Hey there, I’m Giulia!</h1>
+      <h2 className='lg:text-3xl lg:text-left text-center text-lg font-thin mt-12'>
+        <Typewriter
+          onInit={typewriter => {
+            typewriter
+              .typeString("I am passionate about <span class='text-giulia font-normal'>UX design</span>")
+              .pauseFor(1000)
+              .deleteChars(9)
+              .typeString("<span class='text-giulia font-normal'>UI design</span>")
+              .pauseFor(1000)
+              .deleteChars(9)
+              .typeString("<span class='text-giulia font-normal'>Interaction design</span>")
+              .pauseFor(1000)
+              .deleteChars(18)
+              .typeString("<span class='text-giulia font-normal'>UX writing</span>")
+              .start()
+          }}
+          options={{
+            delay: 75
+          }}
+        />
+      </h2>
 
     </div>
-    <img src='/right-arrow.png' alt='go down' className='inset-x-0 mx-auto bottom-0 absolute pb-4' />
+    <img src='/right-arrow.png' alt='go down' className='inset-x-0 mx-auto bottom-0 absolute pb-4 animate-bounce' />
   </div>
 )
 
